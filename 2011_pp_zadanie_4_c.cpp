a94fd26df4c2d74a1c03c3f0c27da8676e699a72
slo-1c-10-2021 <81385155+slo-1c-10-2021@users.noreply.github.com>
2022-03-30 07:44:49 +0200

    Add files via upload

---
+++ 2011_pp_zadanie_4_c.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using  namespace std;
+int main() {
+    string s;
+    ifstream f("hasla.txt");
+    int suma;
+    while (f>>s){
+        for (int i=0; i<s.size()-1; i++) {
+            suma=0;
+            suma=(int)s[i] + (int)s[i+1];
+            if (suma==220) {
+                cout << s << "\n";
+                break;
+            }
+        }
+    }
+}
