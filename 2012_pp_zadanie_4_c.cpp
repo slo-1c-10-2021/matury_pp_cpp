0d3e83dff0eab32563becf34f0298ff0fc2d614b
slo-1c-10-2021 <81385155+slo-1c-10-2021@users.noreply.github.com>
2022-03-30 08:42:49 +0200

    Add files via upload

---
+++ 2012_pp_zadanie_4_c.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using  namespace std;
+bool rosnacy(string s)
+{
+    int p=0;
+    while(p<s.size()-1)
+    {
+        if(s[p+1]<=s[p]) return false;
+        p++;
+    }
+    return true;
+}
+
+int main ()
+{
+    string a;
+    ifstream f("cyfry.txt");
+    while(f>>a)
+    {
+        if(rosnacy(a)) cout << a << endl;
+    }
+}
